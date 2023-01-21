d5471e063f76e04d20b8ccc4236661bb8630dfc0
junior-slv <dirceusilva0422@gmail.com>
2023-01-21 18:58:11 -0300

    added header and home

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header className="header cd">
+        <nav className="nav">
+            <div className="header-logo">
+                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48">
+                <g fill="none" fill-rule="evenodd">
+                    <circle cx="24" cy="24" r="24" fill="#FFF" />
+                    <path
+                    fill="#0B0D17"
+                    d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z"
+                    />
+                </g>
+                </svg>
+            </div>
+            <div className="header_line cd">
+                <span className="header_line-line"></span>
+            </div>
+            <div className="header-nav">
+                <ul className="header-link cd">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#destinos">Destinos</a></li>
+                    <li><a href="#equipe">Equipe</a></li>
+                    <li><a href="#tecnologia">Tecnologia</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home_container cd">
+      <div className="home_content">
+        <div className="home_content-left">
+          <div className="home_content-text">
+            <h4>ENTÃO, VOCÊ QUER VIAJAR PARA O</h4>
+            <h2>ESPAÇO</h2>
+            <p>
+              Convenhamos, se você quer ir para o espaço, pode muito bem ir
+              genuinamente para o espaço sideral e não pairar no limite dele.
+              Bem, sente-se e relaxe porque vamos dar-lhe uma experiência
+              verdadeiramente fora deste mundo!
+            </p>
+          </div>
+        </div>
+        <div className="home_content-right">
+            <div className="home_content-circle cd">
+                <p>EXPLORAR</p>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
